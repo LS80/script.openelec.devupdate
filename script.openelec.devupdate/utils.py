f2fec367e46058bc6150f00384bd2b035bbb18a5
LS80 <github@lee-smith.me.uk>
2012-11-11 17:32:24 +0000

    Added a speed indicator to the progress bar.

--- script.openelec.devupdate/utils.py
+++ script.openelec.devupdate/utils.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 def size_fmt(num):
-    for s, f in (('bytes', '{0:d}'), ('KB', '{0:.1f)'), ('MB', '{0:.1f}')):
+    for s, f in (('bytes', '{0:d}'), ('KB', '{0:.1f}'), ('MB', '{0:.1f}')):
         if num < 1024.0:
             return (f + " {1}").format(num, s)
         num /= 1024.0
