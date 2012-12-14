0c7dbdfebeabf71d0ec05a003b79144576a425bd
LS80 <github@lee-smith.me.uk>
2012-12-14 13:28:00 +0000

    Added the option to install official releases.

--- script.openelec.devupdate/constants.py
+++ script.openelec.devupdate/constants.py
@@ -1,6 +1,18 @@
 import re
 
-CURRENT_BUILD = int(re.search('-r(\d+)', open('/etc/version').read()).group(1))
-ARCH = open('/etc/arch').read().rstrip()
+try:
+    VERSION = open('/etc/version').read()
+except IOError:
+    VERSION = '1.0.0'
+
+try:
+    CURRENT_BUILD = int(re.search('-r(\d+)', VERSION).group(1))
+except AttributeError:
+    CURRENT_BUILD = VERSION.rstrip()
+
+try:
+    ARCH = open('/etc/arch').read().rstrip()
+except IOError:
+    ARCH = 'ION.x86_64'
 
 HEADERS={'User-agent' : "Mozilla/5.0"}
