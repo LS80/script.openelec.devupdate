937dabc0bec8805f4e0b859a063bf6ee8508a748
LS80 <github@lee-smith.me.uk>
2013-05-27 07:17:27 +0100

    Changed the service to use timers instead of a sleep which was causing xbmc to hang on reboot/suspend/shutdown.

--- script.openelec.devupdate/service.py
+++ script.openelec.devupdate/service.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 import xbmc, xbmcgui, xbmcaddon
 
@@ -11,22 +10,26 @@ __addon__ = xbmcaddon.Addon(__scriptid__)
 __icon__ = __addon__.getAddonInfo('icon')
 
 
-source = __addon__.getSetting('source')
-
-if isinstance(INSTALLED_BUILD, Release) and source == "Official Releases":
-    # Don't do the job of the official auto-update system.
-    sys.exit(0)
-
-while (not xbmc.abortRequested):
-    if __addon__.getSetting('check'):
+if not sys.argv[0]:
+    xbmc.executebuiltin("AlarmClock(OpenELECDevUpdate,XBMC.RunScript({}),00:30:00,silent,loop)".format(__file__))
+    if __addon__.getSetting('check') == 'true':
+        xbmc.executebuiltin("AlarmClock(OpenELECDevUpdateFirst,XBMC.RunScript({},started),00:00:30,silent)".format(__file__))
+        interval = int(__addon__.getSetting('check_interval'))
+        xbmc.executebuiltin("AlarmClock(OpenELECDevUpdateCheck,XBMC.RunScript({},started),00:{}:00,silent,loop)".format(__file__, interval))
+    else:
+        xbmc.executebuiltin("CancelAlarm(OpenELECDevUpdateCheck,silent)")
+elif sys.argv[0] and sys.argv[1] == 'started':
+    source = __addon__.getSetting('source')
+    if isinstance(INSTALLED_BUILD, Release) and source == "Official Releases":
+        # Don't do the job of the official auto-update system.
+        pass
+    else:
         try:
             subdir = __addon__.getSetting('subdir')
             if source == "Other":
-                # Custom URL
                 url = __addon__.getSetting('custom_url')
                 build_url = BuildsURL(url, subdir)
             else:
-                # Defined URL
                 build_url = URLS[source]
                 url = build_url.url
     
@@ -35,22 +38,12 @@ while (not xbmc.abortRequested):
                 if latest > INSTALLED_BUILD:
                     if xbmc.Player().isPlayingVideo():
                         xbmc.executebuiltin("Notification(OpenELEC Dev Update, Build {} "
-                                            "is available., 10000, {})".format(latest, __icon__))
+                                            "is available., 7500, {})".format(latest, __icon__))
                     else:   
                         if xbmcgui.Dialog().yesno("OpenELEC Dev Update",
                                                   "A more recent build is available:   {}".format(latest),
                                                   "Show builds available to install?"):
                             xbmc.executebuiltin("RunAddon({})".format(__scriptid__))         
         except:
-            sys.exit(0)
-        
-        interval = float(__addon__.getSetting('check_interval'))
-        time.sleep(interval * 60)
-    else:
-        # Sleep for an hour before checking if the build check is now enabled.
-        time.sleep(360)
-  
-
-    
-    
+            pass
 
