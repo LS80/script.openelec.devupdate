6c5cf53486f93b37ef43d41008ae4c0523e6b7e3
LS80 <github@lee-smith.me.uk>
2013-06-23 20:37:43 +0100

    Organised the imports and some setup code to reduce the delay before the busy indicator shows.

--- script.openelec.devupdate/constants.py
+++ script.openelec.devupdate/constants.py
@@ -1,13 +1,12 @@
 __scriptid__ = 'script.openelec.devupdate'
 
 try:
-    VERSION = open('/etc/version').read().rstrip()
-except IOError:
-    VERSION = '3.0.1'
-
-try:
     ARCH = open('/etc/arch').read().rstrip()
 except IOError:
     ARCH = 'RPi.arm'
+
+if ARCH.startswith('Virtual'):
+    # This just allows easier testing in a virtual machine
+    ARCH = 'RPi.arm'    
 
 HEADERS={'User-agent' : "Mozilla/5.0"}
