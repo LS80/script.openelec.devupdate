82ffce34042edc63267ed8343e559a77c5eb3acb
LS80 <github@lee-smith.me.uk>
2014-09-04 13:26:41 +0100

    Fix printing of file size in bytes.

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -52,7 +52,7 @@ def decompress_error(path, msg):
                         " ", msg)
 
 def size_fmt(num):
-    for s, f in (('bytes', '{0:d}'), ('KB', '{0:.1f}'), ('MB', '{0:.1f}')):
+    for s, f in (('bytes', '{0:.0f}'), ('KB', '{0:.1f}'), ('MB', '{0:.1f}')):
         if num < 1024.0:
             return (f + " {1}").format(num, s)
         num /= 1024.0
