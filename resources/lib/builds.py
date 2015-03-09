2882e3bc35d3e53e0699923e18934d3145286cd9
LS80 <github@lee-smith.me.uk>
2015-03-09 07:54:57 +0000

    Support Milhouse builds with a single letter suffix, e.g. 0308b.

--- resources/lib/builds.py
+++ resources/lib/builds.py
@@ -149,7 +149,7 @@ class ReleaseLink(Release, BuildLinkBase):
 class BuildLinkExtractor(object):
     """Class to extract all the build links from the specified URL"""
 
-    BUILD_RE = ".*OpenELEC.*-{0}-[a-zA-Z]+-(\d+)-(?:r|%23)(\d+)(|-g[0-9a-z]+)\.tar(|\.bz2)"
+    BUILD_RE = ".*OpenELEC.*-{0}-[a-zA-Z]+-(\d+)-(?:r|%23)(\d+[a-z]*)(|-g[0-9a-z]+)\.tar(|\.bz2)"
     CSS_CLASS = None
 
     def __init__(self, url):
